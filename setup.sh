aeb8ae9c7e207e8438cd5f5e0ac0d3b699c75908
Michal Honc <honc355@gmail.com>
2022-01-12 19:53:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-ln -s ~/git/dotfiles/.vim ~/.vim nvim +PlugInstall +qall
+ln -s ~/git/dotfiles/.vim ~/.vim nvim
 ln -s ~/git/dotfiles/.zshrc ~/.zshrc
 ln -s ~/git/dotfiles/oh-my-zsh.sh ~/oh-my-zsh.sh
