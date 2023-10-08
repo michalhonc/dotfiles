05c25dd5823fd7628d9c2a8ae7866e5ed5fe5c18
Michal Honc <honc355@gmail.com>
2023-10-08 10:01:59 +0200

    fix symbol link

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-ln -s ~/git/dotfiles/.vim ~/.vim nvim
+# ln -s ~/git/dotfiles/.vim ~/.vim nvim
+ln -s ~/git/dotfiles/.vim ~/.config nvim
 ln -s ~/git/dotfiles/.zshrc ~/.zshrc
 ln -s ~/git/dotfiles/oh-my-zsh.sh ~/oh-my-zsh.sh
